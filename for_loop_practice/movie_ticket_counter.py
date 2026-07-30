a6b215348c5542faff6d6b948905f92f01271442
Shivangi Yadav <sy379488@gmail.com>
2026-07-30 19:48:24 +0530

    Add files via upload

---
+++ for_loop_practice/movie_ticket_counter.py
@@ -0,0 +1,48 @@
+  # Movie Ticket Counter
+
+'''tickets = 30
+
+while tickets > 0:
+    print("\n=== WELCOME TO MOVIE TICKET COUNTER ===")
+    print(f"Available Tickets: {tickets}")
+
+    customer = int(input("How many tickets do you want? "))
+
+    if customer <= tickets:
+        tickets -= customer
+        print(f"\n🎉 {customer} ticket(s) booked successfully.")
+        print(f"Remaining tickets: {tickets}")
+
+        if tickets == 0:
+            print("\n🎬 House Full!")
+            break
+
+
+    else:
+        print("\n❌ Sorry! Not enough tickets available.")
+Ek tough aur brutal executive advisor
+ ki tarah act karo. Mujhe validate mat karo aur 
+ meri haan mein haan mat milao. Mujhe ek-ek karke 5 
+ aise kathin (tough) sawal poosho jo mere is plan 
+ ke blind spots aur kamiyo ko samne layein. 
+ Jab tak main ek sawal ka jawab na de doon,
+tab tak agla sawal mat poochna".🤷‍♀️🎂✔😉😁💕💕💕💕💕
+
+word=input("Enter a words")
+count=0
+for ch  in word:
+    
+    if ch in "aeiouAEIOU":
+        count+=1
+        print(ch)'''
+Enter_Password_length=8
+password=""
+while Enter_Password_length>password:
+    password=int(input("genrate the passwor "))
+    password.copy()
+    if Enter_Password_length==password:
+        print(password)
+        break
+    else:
+        print("you passwer is worng ")
+
